d3471622017499ef959f7a585d53ab6848d023a5
DenisOlaleye <denis.olaleye@gmail.com>
2022-08-01 16:34:13 +0300

    3.0

--- src/ArithmeticDemo.java
+++ src/ArithmeticDemo.java
@@ -4,23 +4,27 @@ public class ArithmeticDemo
 {
 	public static void main (String [] args)
 	{
-		double firstNumber;
-		double secondNumber;
-		double sum;
-		double difference;
+		int firstNumber;
+		int secondNumber;
+		int sum;
+		int difference;
 		double average;
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Please enter an double >> ");
-		firstNumber = input.nextDouble();
+		System.out.print("Please enter an double      >> ");
+		firstNumber = input.nextInt();
 		System.out.print("Please enter another double >> ");
-		secondNumber = input.nextDouble();
+		secondNumber = input.nextInt();
 		
 		sum = firstNumber + secondNumber;
 		difference = firstNumber - secondNumber;
-		average = sum/2;
-		
+//		average = (double)sum/2; //explicit casting
+		/*average = (double)(sum/2); in this case result of "sum/2" calculated first
+		          and the result is an Integer. Then the whole number casts to a 
+		          double, but the fractional part is already lost.*/
+		average = sum/2.0; /*in this case result is double because of 
+		implicit casting*/
 		System.out.println(firstNumber +" + "+ secondNumber +" is "+ sum);
 		System.out.println(firstNumber +" - "+ secondNumber +" is "+ difference);
 		System.out.println("The average between " +firstNumber+ 
